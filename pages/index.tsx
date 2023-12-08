621b582fa70b8bee8d78bc4a642e3209ed9e3a01
usrtmp <83554162+usrtmp@users.noreply.github.com>
2023-12-08 09:51:52 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Next.js OpenAI Template</title>
+        <title>Small Demo</title>
         <meta
           name="description"
           content="Next.js Template for building OpenAI applications with Supabase."
